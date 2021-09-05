b0c7838d5546d4bec81ac5a144f8e94d85b51748
Yuguo Zhao <cheerzhao77@gmail.com>
2021-09-04 22:21:26 -0500

    add about component to DOM

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { About } from '../models/about';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  animalList: About[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.animalList = [
+      {
+        id: 1,
+        name: 'Panda',
+        fed: true,
+      },
+      {
+        id: 2,
+        name: 'Elephant',
+        fed: true,
+      },
+      {
+        id: 3,
+        name: 'Lion',
+        fed: false,
+      },
+    ];
   }
-
 }
---
+++ src/app/models/about.ts
@@ -0,0 +1,10 @@
+export class About {
+  id: number;
+  name: string;
+  fed: boolean;
+  constructor() {
+    this.id = 1;
+    this.name = '';
+    this.fed = true;
+  }
+}
